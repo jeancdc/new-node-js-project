a529b420009e372726bc5ae359493a6e01079e89
Jean Creuzé des Châtelliers <jean.creuze-des-chatelliers@renault.com>
2021-09-22 15:44:19 +0200

    node version in ci step

--- ci/ci.groovy
+++ ci/ci.groovy
@@ -1,6 +1,12 @@
 pipeline {
     agent any
+    tools {nodejs "NodeJS"}
     stages {
+        stage('Node.js version') {
+            steps {
+                sh 'node -v'
+            }
+        }
         stage('build') {
             steps {
                 echo 'Just a test'
